f177dde2127fa01ea5c591990a3bc9a7add94eb7
caroleonor <54858931+caroleonor@users.noreply.github.com>
2019-10-07 22:50:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,57 +1,57 @@
-import dash
-import dash_core_components as dcc
+import dash #creating a webpage
+import dash_core_components as dcc #visual component
 import dash_html_components as html
-import plotly.graph_objs as go
-from dash.dependencies import Input, Output, State
+import plotly.graph_objs as go #employ graphics
+from dash.dependencies import Input, Output, State #calling the library
 
 ########### Define your variables ######
 
 myheading1='Which Lego piece you will choose?'
-tabtitle = 'Lego!'
-list_of_colors=['2X2', '1X3', '2X4']
-list_of_numbers=['Tile', 'Brick', 'Plate']
+tabtitle = 'Lego Pieces'
+list_of_types=['Brick', 'Plate', 'Tile'] #list_of_colors
+list_of_sizes=['1X2', '2X2', '2X4'] #list_of_numbers
 sourceurl = 'https://github.com/caroleonor/dash-callbacks-multi-input/edit/master/app.py'
 githublink = 'https://brickarchitect.com/book/bricks/'
 
 
 ########## Set up the chart
 
-########### Initiate the app
+########### Initiate the app (setting up html and css)
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets) #for dash to get the url to display the css style sheet
 server = app.server
 app.title=tabtitle
 
 ########### Set up the layout
 
-app.layout = html.Div(children=[
+app.layout = html.Div(children=[ 
     html.H1(myheading1),
     html.Div([
         html.Div([
-            dcc.RadioItems(
-                id='pick-a-color',
+            dcc.RadioItems( #little circle is radio button
+                id='pick-a-type',
                 options=[
-                        {'label':list_of_colors[0], 'value':list_of_colors[0]},
-                        {'label':list_of_colors[1], 'value':list_of_colors[1]},
-                        {'label':list_of_colors[2], 'value':list_of_colors[2]},
+                        {'label':list_of_types[0], 'value':list_of_types[0]},
+                        {'label':list_of_types[1], 'value':list_of_types[1]},
+                        {'label':list_of_types[2], 'value':list_of_types[2]},
                         ],
-                value='choose',
+                value='choose', #"choose" is the action of "user click"
                 ),
         ],className='two columns'),
         html.Div([
             dcc.RadioItems(
-                id='pick-a-number',
+                id='pick-a-size',
                 options=[
-                        {'label':list_of_numbers[0], 'value':list_of_numbers[0]},
-                        {'label':list_of_numbers[1], 'value':list_of_numbers[1]},
-                        {'label':list_of_numbers[2], 'value':list_of_numbers[2]},
+                        {'label':list_of_sizes[0], 'value':list_of_sizes[0]},
+                        {'label':list_of_sizes[1], 'value':list_of_sizes[1]},
+                        {'label':list_of_sizes[2], 'value':list_of_sizes[2]},
                         ],
                 value='one',
                 ),
         ],className='two columns'),
         html.Div([
-            html.Div(id='your_output_here', children=''),
-        ],className='eight columns'),
+            html.Div(id='your_output_here', children=''), #where the output in the html going to be displayed
+        ],className='eight columns'), #calling a section of css format
     ],className='twelve columns'),
     html.Br(),
     html.A('Code on Github', href=githublink),
@@ -63,10 +63,10 @@ app.layout = html.Div(children=[
 ########## Define Callback
 
 @app.callback(Output('your_output_here', 'children'),
-              [Input('pick-a-color', 'value'),
-               Input('pick-a-number', 'value')])
-def radio_results(color_you_picked, number_you_picked):
-    image_you_chose=f'{color_you_picked}-{number_you_picked}.jpg'
+              [Input('pick-a-type', 'value'),
+               Input('pick-a-size', 'value')])
+def radio_results(type_you_picked, size_you_picked):
+    image_you_chose=f'{type_you_picked}-{size_you_picked}.jpg'
     return html.Img(src=app.get_asset_url(image_you_chose), style={'width': 'auto', 'height': 'auto'}),
 
 ############ Deploy
